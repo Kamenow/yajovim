61b6923a00c5a3341a08b6c0dd7d60d50445aa82
Kamenow <iavorkamenov777@gmail.com>
2023-04-29 02:53:02 +0300

    feature: emmet

--- lua/yajobaby/lsp/mason.lua
+++ lua/yajobaby/lsp/mason.lua
@@ -1,51 +1,52 @@
 -- add lsp servers here
 local servers = {
-    "lua_ls",
-    "pyright",
-    "jsonls",
-    "tsserver",
-    "cssmodules_ls",
-    "html"
+	"lua_ls",
+	"pyright",
+	"jsonls",
+	"tsserver",
+	"cssmodules_ls",
+	"html",
+	"emmet_ls",
 }
 
 local settings = {
-    ui = {
-        border = "none",
-        icons = {
-            package_installed = "◍",
-            package_pending = "◍",
-            package_uninstalled = "◍",
-        },
-    },
-    log_level = vim.log.levels.INFO,
-    max_concurrent_installers = 4,
+	ui = {
+		border = "none",
+		icons = {
+			package_installed = "◍",
+			package_pending = "◍",
+			package_uninstalled = "◍",
+		},
+	},
+	log_level = vim.log.levels.INFO,
+	max_concurrent_installers = 4,
 }
 
 require("mason").setup(settings)
 require("mason-lspconfig").setup({
-    ensure_installed = servers,
-    automatic_installation = true,
+	ensure_installed = servers,
+	automatic_installation = true,
 })
 
 local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status_ok then
-    return
+	return
 end
 
 local opts = {}
 
 for _, server in pairs(servers) do
-    opts = {
-        on_attach = require("yajobaby.lsp.handlers").on_attach,
-        capabilities = require("yajobaby.lsp.handlers").capabilities,
-    }
+	opts = {
+		on_attach = require("yajobaby.lsp.handlers").on_attach,
+		capabilities = require("yajobaby.lsp.handlers").capabilities,
+	}
 
-    server = vim.split(server, "@")[1]
+	server = vim.split(server, "@")[1]
 
-    local require_ok, conf_opts = pcall(require, "yajobaby.lsp.settings." .. server)
-    if require_ok then
-        opts = vim.tbl_deep_extend("force", conf_opts, opts)
-    end
+	local require_ok, conf_opts = pcall(require, "yajobaby.lsp.settings." .. server)
+	if require_ok then
+		opts = vim.tbl_deep_extend("force", conf_opts, opts)
+	end
 
-    lspconfig[server].setup(opts)
+	lspconfig[server].setup(opts)
 end
